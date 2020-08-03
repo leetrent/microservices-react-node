f92b535ef8341d50b807195915f4eb4c7adb557e
LeeTrent <lee.trent.1@gmail.com>
2020-08-03 09:01:44 -0500

    Implements query service

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { request } = require('express');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (request, response) => {
+const posts = {};
 
+app.get('/posts', (request, response) => {
+    response.send(posts);
 });
 
 app.post('/events', (request, response) => {
+    
+    const { type, data } = request.body;
+    
+    if ( type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
 
+    if ( type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    console.log(posts);
+    response.send({});
 });
 
 app.listen(4002, () => {
