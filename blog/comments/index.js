54cae725a43064662e4d863298f7e05fe9e0bf36
LeeTrent <lee.trent.1@gmail.com>
2020-08-05 10:23:02 -0500

    Wiring up moderation service

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -18,21 +18,35 @@ app.post('/posts/:id/comments', async (request, response) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = request.body;
     const comments = commentsByPostId[request.params.id] || [];
-    comments.push( {id: commentId, content});
+    comments.push( {id: commentId, content, status: 'pending'});
     commentsByPostId[request.params.id] = comments;
     await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
             content,
-            postId: request.params.id
+            postId: request.params.id,
+            status: 'pending'
         }
     });
     response.status(201).send(comments);
 });
 
-app.post('/events', (request, response)  =>{
+app.post('/events', async (request, response)  =>{
     console.log(`Comments Service received the following event '${request.body.type}'`);
+    const { type, data } = request.body;
+    if (type === 'CommentModerated') {
+        const { postId, id, status } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: { id, status, postId, content }
+        });
+    }
     response.send({});
 });
 
