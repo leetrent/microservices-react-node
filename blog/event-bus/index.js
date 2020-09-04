f28729c07eef5de27c5b5e1af4bf1818f10b2175
LeeTrent <Lee.Trent.1@gmail.com>
2020-09-04 09:21:56 -0500

    Updates event-bus to use Kubernetes IPs when sending notifications to other microservices

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -12,9 +12,9 @@ app.post('/events', (request, response) => {
     events.push(event);
 
     axios.post('http://posts-clusterip-srv:4000/events', event);
-    // axios.post('http://localhost:4001/events', event);
-    // axios.post('http://localhost:4002/events', event);
-    // axios.post('http://localhost:4003/events', event);
+    axios.post('http://comments-srv:4001/events', event);
+    axios.post('http://query-srv:4002/events', event);
+    axios.post('http://moderation-srv:4003/events', event);
 
     response.send({ status: 'OK' });
 });
